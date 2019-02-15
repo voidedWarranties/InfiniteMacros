d425e8aa8f19f734767d2fc4efcdc164dd70d7bd
voidedWarranties <wz68.scratch@gmail.com>
2019-02-15 02:31:29 +0000

    configuration!

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ print("Avoid pressing keys during startup. Wait until OK for all devices.")
 print("Pressing keys during startup causes those keys to get locked up until the device is unplugged.")
 print("Press [Esc] to exit")
 
-async def print_events(device):
+async def print_events(device, config):
     print("OK: ", device.path)
     while True:
         async for event in device.async_read_loop():
@@ -22,7 +22,8 @@ async def print_events(device):
                 else:
                     if(keyevt.keystate < 2):
                         # print(device.path, keyevt)
-                        url = "http://192.168.1.163"
+                        url = str(config["ip"])
+                        print(url)
                         path = "/"
                         
                         params = {
@@ -47,6 +48,10 @@ def ext():
 
 devices = [evdev.InputDevice("/dev/Macros1")]
 
+config = open("config.json")
+config_str = config.read()
+config_data = json.loads(config_str)
+
 for device in devices:
     # Does device.capabilities[EV_KEY] not contain BTN_MOUSE?
     if 272 not in device.capabilities()[1]:
@@ -54,7 +59,7 @@ for device in devices:
             device.grab()
         except:
             pass
-        asyncio.ensure_future(print_events(device))
+        asyncio.ensure_future(print_events(device, config_data))
 
 loop = asyncio.get_event_loop()
 loop.run_forever()
